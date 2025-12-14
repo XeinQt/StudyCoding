32bb99343c4ec2acdb176b4556dc2d553fe5cacf
My Name <ricoalentijo4@gmail.com>
2025-12-14 10:17:37 +0800

    arrays

---
+++ js/array.js
@@ -0,0 +1,10 @@
+const cars = ["haha", "hehe", "huhu"];
+
+cars.push("motor");
+
+console.log(cars);
+console.log(cars[0]);
+console.log(cars[cars.length - 1]);
+
+cars.splice(1, 1);
+console.log(cars);

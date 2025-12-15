b1ba93fe4864ea19f37db3f332ed0b6a2d664253
My Name <ricoalentijo4@gmail.com>
2025-12-15 08:11:50 +0800

    arrays iterations

--- js/array.js
+++ js/array.js
@@ -1,10 +1,45 @@
 const cars = ["haha", "hehe", "huhu"];
 
-cars.push("motor");
+// cars.push("motor");
 
-console.log(cars);
-console.log(cars[0]);
-console.log(cars[cars.length - 1]);
+// console.log(cars);
+// console.log(cars[0]);
+// console.log(cars[cars.length - 1]);
 
-cars.splice(1, 1);
-console.log(cars);
+// cars.splice(1, 1);
+// console.log(cars);
+
+// if (cars.includes("motor")) {
+//   console.log(`cars has motor`);
+// }
+
+// console.log("hello lord");
+
+let text = "";
+
+cars.forEach(myFunction);
+
+function myFunction(value, index, array) {
+  text += value;
+}
+
+// console.log(text);
+
+const number1 = [1, 23, 4, 5, 56, 67];
+const number2 = number1.map(mapping);
+
+function mapping(value, index, array) {
+  return value * 4;
+}
+
+// console.log(number2);
+
+const ages = [10, 23, 53, 123, 54, 43];
+
+const filterAge = ages.filter(checkingTheAge);
+
+function checkingTheAge(value, index, array) {
+  return value > 18;
+}
+
+console.log(filterAge);
